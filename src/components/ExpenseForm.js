238e649fbd8647bf5a811e6c65fe89870f72d11d
Lawrence <Caulker>
2020-05-07 16:44:22 -0400

    completed UI setup

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,10 +1,24 @@
-import React from "react"
+import React from 'react'
+import { MdSend } from 'react-icons/md'
 
 const ExpenseForm = () => {
 	return (
-		<div>
-			<p>Hello from Express form</p>
-		</div>
+		<form>
+			<div className="form-center">
+				<div className="form-group">
+					<label htmlFor="charge">Charge</label>
+					<input type="text" className="form-control" id="charge" name="charge" placeholder="eg. rent" />
+				</div>
+				<div className="form-group">
+					<label htmlFor="amount">Amount</label>
+					<input type="text" className="form-control" id="amount" name="amount" placeholder="eg. 100" />
+				</div>
+			</div>
+			<button type="submit" className="btn">
+				Submit
+				<MdSend className="btn-icon" />
+			</button>
+		</form>
 	)
 }
 
