780eb4ba0e4c64af4f5c980ca3ab34f7df670d90
Lawrence <locaulker@gmail.com>
2020-05-08 01:00:27 -0400

    added expense form props

--- src/App.js
+++ src/App.js
@@ -1,29 +1,51 @@
-import React, { useState } from 'react'
-import './App.css'
-import ExpenseList from './components/ExpenseList'
-import ExpenseForm from './components/ExpenseForm'
-import Alert from './components/Alert'
-import { v4 as uuidv4 } from 'uuid'
+import React, { useState } from "react"
+import "./App.css"
+import ExpenseList from "./components/ExpenseList"
+import ExpenseForm from "./components/ExpenseForm"
+import Alert from "./components/Alert"
+import { v4 as uuidv4 } from "uuid"
 
 const initialExpenses = [
-	{ id: uuidv4(), charge: 'rent', amount: 1600 },
-	{ id: uuidv4(), charge: 'car payment', amount: 400 },
-	{ id: uuidv4(), charge: 'credit card bill', amount: 1200 },
+	{ id: uuidv4(), charge: "rent", amount: 1600 },
+	{ id: uuidv4(), charge: "car payment", amount: 400 },
+	{ id: uuidv4(), charge: "credit card bill", amount: 1200 },
 ]
 
 const App = () => {
+	// *********** State values ************
 	const [expenses, setExpenses] = useState(initialExpenses)
+	const [charge, setCharge] = useState("")
+	const [amount, setAmount] = useState("")
+
+	// *********** Functionalities ************
+	const handleCharge = (e) => {
+		console.log(`charge: ${e.target.value}`)
+		setCharge(e.target.value)
+	}
+	const handleAmount = (e) => {
+		console.log(`amount: ${e.target.value}`)
+		setAmount(e.target.value)
+	}
+	const handleSubmit = (e) => {
+		e.preventDefault()
+	}
 
 	return (
 		<>
 			<Alert />
 			<h1>Budget Calculator</h1>
 			<main className="App">
-				<ExpenseForm />
+				<ExpenseForm 
+					charge={charge} 
+					amount={amount} 
+					handleCharge={handleCharge} 
+					handleAmount={handleAmount} 
+					handleSubmit={handleSubmit} 
+				/>
 				<ExpenseList expenses={expenses} />
 			</main>
 			<h1>
-				Total Spending:{' '}
+				Total Spending:{" "}
 				<span className="total">
 					$
 					{expenses.reduce((acc, curr) => {
--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,17 +1,38 @@
-import React from 'react'
-import { MdSend } from 'react-icons/md'
+import React from "react"
+import { MdSend } from "react-icons/md"
 
-const ExpenseForm = () => {
+const ExpenseForm = ({ 
+	charge, 
+	amount, 
+	handleCharge, 
+	handleAmount, 
+	handleSubmit 
+}) => {
 	return (
-		<form>
+		<form onSubmit={handleSubmit}>
 			<div className="form-center">
 				<div className="form-group">
 					<label htmlFor="charge">Charge</label>
-					<input type="text" className="form-control" id="charge" name="charge" placeholder="eg. rent" />
+					<input 
+						type="text" 
+						className="form-control" 
+						id="charge" 
+						name="charge" 
+						placeholder="eg. rent" 
+						value={charge}
+						onChange={handleCharge}
+					/>
 				</div>
 				<div className="form-group">
 					<label htmlFor="amount">Amount</label>
-					<input type="text" className="form-control" id="amount" name="amount" placeholder="eg. 100" />
+					<input 
+						type="number" 
+						className="form-control" 
+						id="amount" name="amount" 
+						placeholder="eg. 100"
+						value={amount}
+						onChange={handleAmount}
+					/>
 				</div>
 			</div>
 			<button type="submit" className="btn">
