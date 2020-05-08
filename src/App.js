6743e0543998aa674f34c3e748f97a5246fcb71c
Lawrence <locaulker@gmail.com>
2020-05-08 06:44:16 -0400

    added handleSubmit functionality

--- src/App.js
+++ src/App.js
@@ -6,55 +6,62 @@ import Alert from "./components/Alert"
 import { v4 as uuidv4 } from "uuid"
 
 const initialExpenses = [
-	{ id: uuidv4(), charge: "rent", amount: 1600 },
-	{ id: uuidv4(), charge: "car payment", amount: 400 },
-	{ id: uuidv4(), charge: "credit card bill", amount: 1200 },
+  { id: uuidv4(), charge: "rent", amount: 1600 },
+  { id: uuidv4(), charge: "car payment", amount: 400 },
+  { id: uuidv4(), charge: "credit card bill", amount: 1200 },
 ]
 
 const App = () => {
-	// *********** State values ************
-	const [expenses, setExpenses] = useState(initialExpenses)
-	const [charge, setCharge] = useState("")
-	const [amount, setAmount] = useState("")
+  // *********** State values ************
+  const [expenses, setExpenses] = useState(initialExpenses)
+  const [charge, setCharge] = useState("")
+  const [amount, setAmount] = useState("")
 
-	// *********** Functionalities ************
-	const handleCharge = (e) => {
-		console.log(`charge: ${e.target.value}`)
-		setCharge(e.target.value)
-	}
-	const handleAmount = (e) => {
-		console.log(`amount: ${e.target.value}`)
-		setAmount(e.target.value)
-	}
-	const handleSubmit = (e) => {
-		e.preventDefault()
-	}
+  // *********** Functionalities ************
+  const handleCharge = (e) => {
+    setCharge(e.target.value)
+  }
+  const handleAmount = (e) => {
+    setAmount(e.target.value)
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
 
-	return (
-		<>
-			<Alert />
-			<h1>Budget Calculator</h1>
-			<main className="App">
-				<ExpenseForm 
-					charge={charge} 
-					amount={amount} 
-					handleCharge={handleCharge} 
-					handleAmount={handleAmount} 
-					handleSubmit={handleSubmit} 
-				/>
-				<ExpenseList expenses={expenses} />
-			</main>
-			<h1>
-				Total Spending:{" "}
-				<span className="total">
-					$
-					{expenses.reduce((acc, curr) => {
-						return (acc += curr.amount)
-					}, 0)}
-				</span>
-			</h1>
-		</>
-	)
+    if (charge !== "" && amount > 0) {
+      const singleExpense = { id: uuidv4(), charge, amount }
+      setExpenses([...expenses, singleExpense])
+      setCharge("")
+      setAmount("")
+    } else {
+      // call handleAlert
+    }
+  }
+
+  return (
+    <>
+      <Alert />
+      <h1>Budget Calculator</h1>
+      <main className="App">
+        <ExpenseForm
+          charge={charge}
+          amount={amount}
+          handleCharge={handleCharge}
+          handleAmount={handleAmount}
+          handleSubmit={handleSubmit}
+        />
+        <ExpenseList expenses={expenses} />
+      </main>
+      <h1>
+        Total Spending:{" "}
+        <span className="total">
+          $
+          {expenses.reduce((acc, curr) => {
+            return (acc += parseInt(curr.amount))
+          }, 0)}
+        </span>
+      </h1>
+    </>
+  )
 }
 
 export default App
--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,46 +1,47 @@
 import React from "react"
 import { MdSend } from "react-icons/md"
 
-const ExpenseForm = ({ 
-	charge, 
-	amount, 
-	handleCharge, 
-	handleAmount, 
-	handleSubmit 
+const ExpenseForm = ({
+  charge,
+  amount,
+  handleCharge,
+  handleAmount,
+  handleSubmit,
 }) => {
-	return (
-		<form onSubmit={handleSubmit}>
-			<div className="form-center">
-				<div className="form-group">
-					<label htmlFor="charge">Charge</label>
-					<input 
-						type="text" 
-						className="form-control" 
-						id="charge" 
-						name="charge" 
-						placeholder="eg. rent" 
-						value={charge}
-						onChange={handleCharge}
-					/>
-				</div>
-				<div className="form-group">
-					<label htmlFor="amount">Amount</label>
-					<input 
-						type="number" 
-						className="form-control" 
-						id="amount" name="amount" 
-						placeholder="eg. 100"
-						value={amount}
-						onChange={handleAmount}
-					/>
-				</div>
-			</div>
-			<button type="submit" className="btn">
-				Submit
-				<MdSend className="btn-icon" />
-			</button>
-		</form>
-	)
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="form-center">
+        <div className="form-group">
+          <label htmlFor="charge">Charge</label>
+          <input
+            type="text"
+            className="form-control"
+            id="charge"
+            name="charge"
+            placeholder="Enter name of charge"
+            value={charge}
+            onChange={handleCharge}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="amount">Amount</label>
+          <input
+            type="number"
+            className="form-control"
+            id="amount"
+            name="amount"
+            placeholder="Enter Amount"
+            value={amount}
+            onChange={handleAmount}
+          />
+        </div>
+      </div>
+      <button type="submit" className="btn">
+        Submit
+        <MdSend className="btn-icon" />
+      </button>
+    </form>
+  )
 }
 
 export default ExpenseForm
